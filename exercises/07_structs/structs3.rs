5186ac7a2eafffdfbb0a90f3dac954b952573a38
Enzo <enzo.marini888@gmail.com>
2024-01-18 13:02:53 +0100

    Exo 7

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -7,7 +7,22 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+// WRITEUP
+// Pour cet exercice, il nous était demandé de complèter les fonctions
+// `is_international` et `get_fees` 
+// Pour `is_international`, la fonction doit retourner un booléen si le pays
+// émetteur est le même que le pays destinataire du colis ou non et a pour
+// argument `&self`. 
+// J'ai d'abord récupéré les variables `sender_country` et `recipient_country`
+// grâce à `&self`, je les ai comparé et retourné faux si les pays étaient les
+// même et vrai s'ils étaient diférents
+// Pour la fonction `get_fees`, j'ai dans un premier temps récupérer le poid en
+// grammes du colis grâce au `&self` mis en argument de la fonction et j'ai
+// retourné le calcul des frais de ports en multipliant la masse et le prix par
+// gramme. 
+// Un `u32` était attendu pour le retour de la fonction étant donné que la
+// variable `cents_per_gram` mis en argument était définie en `u32`.
 
 #[derive(Debug)]
 struct Package {
@@ -31,12 +46,21 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        let sender = &self.sender_country;
+        let recipient = &self.recipient_country;
+        if sender == recipient {
+            false
+        } else {
+            true
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
         // Something goes here...
+        let weight = &self.weight_in_grams;
+        weight*cents_per_gram
     }
 }
 
