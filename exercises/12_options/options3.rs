617cae6e8a4d79590d71ddef4c54aad9db497b08
Enzo <enzo.marini888@gmail.com>
2024-01-18 18:53:14 +0100

    Exo 12

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -3,7 +3,15 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+// WRITEUP
+// Il y a une erreur au niveau de ka variable `p` à cette ligne :
+// Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+// car la variable est déplacé et réutilisé plus tard et comme elle est de type
+// `Point`, il n'est pas possible d'implémenter le trait `Copy`.
+// Pour résoudre cela il faut utilisé le mot clé `ref` afin d'emprunté la valeur
+// plutôt que de la déplacé.
+
 
 struct Point {
     x: i32,
@@ -14,7 +22,7 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
