03e442a9539dcd766c9eea6bbb352c996e63d267
Enzo <enzo.marini888@gmail.com>
2024-01-18 16:01:11 +0100

    Exo 9

--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -7,7 +7,33 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
+
+// WRITEUP
+// Dans cet exercice, il fallait ajouter `string_slice` si le paramètre est un
+// `&str` ou `string` si le paramètre est un `String`.
+// Pour `"blue"`, c'est un `&str` donc je lui ai ajouté `string_slice`.
+// Pour `"red".to_string()`, il y a une conversion avec `.to_string()` donc
+// c'est un `String`.
+// Pour `String::from("hi")`, il y a une conversion avec `String::from()` donc
+// c'est un `String`.
+// Pour `"rust is fun!".to_owned()`, il y a un `to_owned()` qui crée une copie
+// d'un `&str` vers un `String`, donc c'est un `String`.
+// Pour `"nice weather".into()`, il y a un `.into()` qui converti un entier en
+// `String`, donc c"est un `String`.
+// Pour `format!("Interpolation {}", "Station")`, il y a la macro `format!()`
+// qui crée une nouvelle chaine de caratères `String` a partir de ses arguments,
+// donc c'est un `string`.
+// Pour `&String::from("abc")[0..1]`, il y a une plage d'indice allant de 0 à 1,
+// c'est donc un `&str`.
+// Pour `"  hello there ".trim()`, il y a un `.trim()` qui renvoie un `&str`
+// après avoir supprimé les espaces en trop, c'est donc un `&str`.
+// Pour `"Happy Monday!".to_string().replace("Mon", "Tues")`, il y a une
+// conversion avec `.to_string()` et `.replace()` renvoie un `String` après avoir
+// remplacé un élément donc c'est un `String`.
+// Pour `"mY sHiFt KeY iS sTiCkY".to_lowercase()`, il ya un `.to_lowercase()`
+// qui renvoie un `String` après avoir mis tous les caractères de la chaine en
+// minuscule, c'est donc un `String`.
+
 
 fn string_slice(arg: &str) {
     println!("{}", arg);
@@ -17,14 +43,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
+    string("nice weather".into());
+    string(format!("Interpolation {}", "Station"));
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
